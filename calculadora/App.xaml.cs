e83fb8215ad1f561cc9f9e24c760efbcc3fb9b88
Michael Portela <mportela19900@gmail.com>
2024-10-21 20:18:46 -0500

    ambiente creado

---
+++ calculadora/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace calculadora;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ calculadora/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace calculadora;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
